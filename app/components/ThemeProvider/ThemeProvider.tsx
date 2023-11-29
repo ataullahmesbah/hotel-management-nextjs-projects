4ab04286287370f73f39cd2ded85f55886850d51
ataullahmesbah <mesbahataullah4@gmail.com>
2023-11-29 21:29:24 +0600

    ThemeProvider set

--- app/components/ThemeProvider/ThemeProvider.tsx
+++ app/components/ThemeProvider/ThemeProvider.tsx
@@ -5,11 +5,22 @@ import { useEffect, useState } from "react";
 import ThemeContext from "@/app/context/themeContext";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-    const themeFromStorage =
+    const themeFromStorage: boolean =
         typeof localStorage !== 'undefined' && localStorage.getItem('hotel-theme')
             ? JSON.parse(localStorage.getItem('hotel-theme')!)
             : false;
 
 
-    const [darkTheme, setDarkTheme] = useState<boolean>();
-}
+    const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+        <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+            <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+                {children}
+            </div>
+        </div>
+    </ThemeContext.Provider>
+
+};
+
+export default ThemeProvider;
