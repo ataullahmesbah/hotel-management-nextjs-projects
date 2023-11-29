3d5739178d4023c813ce9239dc9dc0dd3eb11dd8
ataullahmesbah <mesbahataullah4@gmail.com>
2023-11-29 19:11:28 +0600

    Footer setup and css set complete

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="mt-16">
+            <div className="container mx-auto px-4">
+
+                
+            </div>
+            
+        </footer>
+    );
+};
+
+export default Footer;
