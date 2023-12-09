5c9c2bb91b636c45a6eae1177ace7c8124216503
ataullahmesbah <mesbahataullah4@gmail.com>
2023-12-09 19:43:21 +0600

    Page and pro

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -37,7 +37,7 @@ const Auth = () => {
 
     };
 
-
+// 
     return (
         <section className="container mx-auto">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
