922f565d9c0fc3ad033b81fe600217e27d3876f7
ataullahmesbah <mesbahataullah4@gmail.com>
2023-12-09 19:30:37 +0600

    Sign up formate

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,42 @@
+'use client';
+
+
+
+
+import { trace } from 'console';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 
+
+const defaultFormData = {
+    email: '',
+    name: '',
+    password: '',
+}
+
 const Auth = () => {
+    const [formData, setFormData] = useState(defaultFormData);
     const inputStyles = "border border-gray-300 sm:text-sm text-black rounded:lg block w-full p-2.5 focus:outline-none"
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            console.log(formData)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setFormData(defaultFormData);
+        }
+
+    };
+
 
     return (
         <section className="container mx-auto">
@@ -17,31 +51,34 @@ const Auth = () => {
                         <FcGoogle className="ml-3 text-4xl cursor-pointer" />
                     </span>
                 </div>
-                <form className="space-y-4 md:space-y-6">
+                <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                     <input
                         type="email"
                         name='email'
-                        id='email'
                         placeholder='name@info.com'
                         required
                         className={inputStyles}
+                        value={formData.email}
+                        onChange={handleInputChange}
                     />
                     <input
                         type="text"
                         name='name'
-                        id='name'
                         placeholder='Ataullah Mesbah'
                         required
                         className={inputStyles}
+                        value={formData.name}
+                        onChange={handleInputChange}
                     />
                     <input
                         type="password"
                         name='password'
-                        id='password'
                         placeholder='password'
                         required
                         minLength={8}
                         className={inputStyles}
+                        value={formData.password}
+                        onChange={handleInputChange}
                     />
                     <button type="submit" className='w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Sign Up</button>
                 </form>
